d380c62174541af9f1e3aa0ac299b66893dd6a23
Buckley Ross <brr322@lehigh.edu>
2020-11-07 16:41:45 -0500

    feat(multithread): add uncapped multithreading
    This isn't a good solution, but it's a good start towards one. We
    definitely need to cap this later, probably using a thread pool.
    
    Signed-off-by: Buckley Ross <brr322@lehigh.edu>
    Change-Id: I64fa163a7c2808d1dc6b6f9155011096534dbd55

--- IntSortingMethods/GrossSort.java
+++ IntSortingMethods/GrossSort.java
@@ -1,9 +1,70 @@
 package IntSortingMethods;
 
 public class GrossSort extends Sort {
+	/* Define a class which can serve as our thread runner: */
+	private class SortThread extends Thread {
+		private int data[];
+		private int start;
+		private int len;
 
+		/* Define a constructor to set all of the internal fields: */
+		public SortThread(int data[], int start, int len) {
+			this.data = data;
+			this.start = start;
+			this.len = len;
+		}
+
+		/* Perform the partitioning step of QuickSort: */
+		@Override public void run() {
+			// Handle the base case:
+			if(len - start < 2) {
+				return;
+			}
+
+			// TODO: Select an optimal pivot point:
+			int pivot = start;
+
+			// Perform the necessary swap operations:
+			for(int i = start; i < len; ++i) {
+				if(data[i] < data[pivot]) {
+					int tmp = data[pivot];
+					data[pivot] = data[i];
+					data[i] = data[pivot + 1];
+					data[pivot + 1] = tmp;
+					++pivot;
+				}
+			}
+
+			/* Spawn a child thread to process 50% of the remaining work: */
+			/* TODO: Don't spawn a thread if that thread's base-case is met */
+			SortThread t1 = new SortThread(data, start, pivot);
+			t1.start();
+
+			/* Recursively process the next section: */
+			start = pivot + 1;
+			run();
+
+			/* Attempt to join the child thread: */
+			try {
+				t1.join();
+			} catch(InterruptedException e) {
+				System.err.println("ERROR: Sorting thread was interrupted unexpectedly");
+				System.exit(1);
+			}
+		}
+	}
+
+	/* Spawn a new thread to run the sorting algorithm on the provided array: */
+	/* TODO: The main thread should be included in the thread pool, and shouldn't just be waiting around here */
 	void algorithm() {
-		sort(0, data.length);
+		SortThread sorter = new SortThread(data, 0, data.length);
+		sorter.start();
+		try {
+			sorter.join();
+		} catch(InterruptedException e) {
+			System.err.println("ERROR: Sorting thread was interrupted unexpectedly");
+			System.exit(1);
+		}
 	}
 
 	void linearPass(int[] arr) {
@@ -13,34 +74,8 @@ public class GrossSort extends Sort {
 		}
 	}
 
-	/* Perform the partitioning step of QuickSort: */
-	public void sort(int start, int len) {
-		// Handle the base case:
-		if(len - start < 2) {
-			return;
-		}
-
-		// TODO: Select an optimal pivot point:
-		int pivot = start;
-
-		// Perform the necessary swap operations:
-		for(int i = start; i < len; ++i) {
-			if(data[i] < data[pivot]) {
-				int tmp = data[pivot];
-				data[pivot] = data[i];
-				data[i] = data[pivot + 1];
-				data[pivot + 1] = tmp;
-				++pivot;
-			}
-		}
-
-		sort(start, pivot);
-		sort(pivot + 1, len);
-	}
-
 	public String getAuthor() {
 		return "crg222 and brr322";
 	}
-
 }
 
